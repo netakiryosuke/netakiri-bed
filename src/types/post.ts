f3ef1f3a10d8d02ef0127137689440a62d007cb2
netakiryosuke <sigoryosuke@gmail.com>
2026-04-13 22:00:44 +0900

    Add Post type definitions

---
+++ src/types/post.ts
@@ -0,0 +1,11 @@
+export interface PostFrontmatter {
+  title: string;
+  date: string;
+  tags: string[];
+  description?: string;
+}
+
+export interface Post extends PostFrontmatter {
+  slug: string;
+  content: string;
+}
