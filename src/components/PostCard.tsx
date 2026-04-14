1c8982c980416931a01d5b39a1240141de41f959
netakiryosuke <sigoryosuke@gmail.com>
2026-04-14 21:09:10 +0900

    Move Link inside h2 and guard empty tags list

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -8,18 +8,20 @@ interface Props {
 export default function PostCard({ post }: Props) {
   return (
     <article>
-      <Link href={`/posts/${post.slug}`}>
-        <h2>{post.title}</h2>
-      </Link>
+      <h2>
+        <Link href={`/posts/${post.slug}`}>{post.title}</Link>
+      </h2>
       <time dateTime={post.date}>{post.date}</time>
       {post.description && <p>{post.description}</p>}
-      <ul>
-        {post.tags.map((tag) => (
-          <li key={tag}>
-            <Link href={`/tags/${tag}`}>{tag}</Link>
-          </li>
-        ))}
-      </ul>
+      {post.tags.length > 0 && (
+        <ul>
+          {post.tags.map((tag) => (
+            <li key={tag}>
+              <Link href={`/tags/${tag}`}>{tag}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </article>
   );
 }
