05e3e5e4a4aa9955b1a38c7ff7dd04d94e5d1a57
netakiryosuke <sigoryosuke@gmail.com>
2026-04-14 21:00:32 +0900

    Add post list page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import PostCard from "@/components/PostCard";
+import { getAllPosts } from "@/lib/posts";
+
 export default function Home() {
+  const posts = getAllPosts();
+
   return (
-    <main className="flex flex-1 items-center justify-center">
-      <p>Hello, world!</p>
+    <main>
+      <h1>記事一覧</h1>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.slug}>
+            <PostCard post={post} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
