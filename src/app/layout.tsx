ee45ea02a0160768b408134e8919b4efe013f65e
netakiryosuke <sigoryosuke@gmail.com>
2026-04-15 22:40:28 +0900

    Add background image to layout and update styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="ja"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-[url('/background.png')] bg-cover">
         <Header />
         {children}
       </body>
