ac03c70ab1e13511fa0127cc7c3c2a1b5acb8715
netakiryosuke <sigoryosuke@gmail.com>
2026-04-15 23:47:05 +0900

    Remove redundant classes from Header component links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,12 +4,12 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <Link href="/" className="text-lg text-white">Netakiri Blog</Link>
+      <Link href="/">Netakiri Blog</Link>
       <nav aria-label="Primary">
         <ul className="flex flex-row gap-4">
-          <li><Link href="/" className="text-white">Home</Link></li>
-          <li><Link href="/tags" className="text-white">Tags</Link></li>
-          <li><Link href="/about" className="text-white">About</Link></li>
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/tags">Tags</Link></li>
+          <li><Link href="/about">About</Link></li>
         </ul>
       </nav>
     </header>
