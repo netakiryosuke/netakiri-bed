209e92b8dd89fda4409151c3a8ee3e47eccaf6f6
netakiryosuke <sigoryosuke@gmail.com>
2026-04-15 23:36:09 +0900

    Fix indentation in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 
 export default function Header() {
-    return (
-        <header className={styles.header}>
-            <Link href="/" className="text-lg text-white">Netakiri Blog</Link>
-            <nav aria-label="Primary">
-                <ul className="flex flex-row gap-4">
-                    <li><Link href="/" className="text-white">Home</Link></li>
-                    <li><Link href="/tags" className="text-white">Tags</Link></li>
-                    <li><Link href="/about" className="text-white">About</Link></li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className={styles.header}>
+      <Link href="/" className="text-lg text-white">Netakiri Blog</Link>
+      <nav aria-label="Primary">
+        <ul className="flex flex-row gap-4">
+          <li><Link href="/" className="text-white">Home</Link></li>
+          <li><Link href="/tags" className="text-white">Tags</Link></li>
+          <li><Link href="/about" className="text-white">About</Link></li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
