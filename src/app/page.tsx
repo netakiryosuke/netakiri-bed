cd8d0597659726ed412f3d3441f8f82f48807e98
netakiryosuke <sigoryosuke@gmail.com>
2026-04-15 20:13:01 +0900

    fixup! Add header to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from "@/components/Header";
 import PostCard from "@/components/PostCard";
 import { getAllPosts } from "@/lib/posts";
 
@@ -7,7 +6,6 @@ export default function Home() {
 
   return (
     <>
-      <Header />
       <main>
         <h1>記事一覧</h1>
         <ul>
