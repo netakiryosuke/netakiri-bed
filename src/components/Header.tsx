630027b2a845f72c1c4b3b9999da28ac41624995
netakiryosuke <sigoryosuke@gmail.com>
2026-04-15 20:13:13 +0900

    Update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 export default function Header() {
     return (
-        <header>
-            <h1>My Blog</h1>
+        <header className="flex justify-between items-center px-6 py-6">
+            <a href="/" className="text-lg">Netakiri Blog</a>
+            <div className="flex flex-row gap-4">
+                <a href="/">Home</a>
+                <a href="/tags">Tags</a>
+                <a href="/about">About</a>
+            </div>
         </header>
     );
 }
