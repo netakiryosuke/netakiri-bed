43f9ecfc71b20ed93c36a33853c3ccf6084e6831
netakiryosuke <sigoryosuke@gmail.com>
2026-04-18 17:56:30 +0900

    Integrate TagsDropdown into Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 import styles from "./Header.module.css";
+import TagsDropdown from "./TagsDropdown";
+import { getAllTags } from "@/lib/posts";
 
 export default function Header() {
+  const tags = getAllTags();
+
   return (
     <header className={styles.header}>
       <Link href="/">Netakiri Blog</Link>
       <nav aria-label="Primary">
-        <ul className="flex flex-row gap-4">
+        <ul className="flex flex-row gap-4 items-center">
           <li><Link href="/">Home</Link></li>
-          <li><Link href="/tags">Tags</Link></li>
+          <li><TagsDropdown tags={tags} /></li>
           <li><Link href="/about">About</Link></li>
         </ul>
       </nav>
