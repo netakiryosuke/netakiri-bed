b92a314efabac3ab00360a4998e17e928e10d25a
netakiryosuke <sigoryosuke@gmail.com>
2026-04-18 18:59:52 +0900

    Refactor AboutPage layout by removing sidebar and adjusting styles

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -13,21 +13,10 @@ export default async function AboutPage() {
   return (
     <main>
       <article className="text-white p-10">
-        <div className="flex gap-8 py-10">
-          <div dangerouslySetInnerHTML={{ __html: contentHtml }} className={`p-8 pt-0 border border-white rounded flex-[3] bg-white/3 backdrop-blur-sm ${styles.content}`} />
-          <aside className="
-            sticky
-            top-24
-            self-start
-            border border-transparent
-            rounded
-            flex-[1]
-            text-black
-            bg-white/90
-          ">
-            <Toc items={toc} />
-          </aside>
-        </div>
+        <div
+          dangerouslySetInnerHTML={{ __html: contentHtml }}
+          className={`p-8 pt-4 border border-white rounded flex-[3] bg-white/3 backdrop-blur-sm ${styles.content}`}
+        />
       </article>
     </main>
   );
