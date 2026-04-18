0f3a29184f5277ee211b766391e775580b305540
netakiryosuke <sigoryosuke@gmail.com>
2026-04-18 23:16:21 +0900

    Update header to use Japanese and English titles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,13 @@ export default function Header() {
 
   return (
     <header className={styles.header}>
-      <Link href="/">Netakiri Blog</Link>
+      <Link href="/" className="flex gap-2">
+        <span className="font-jp text-xl">寝たきり</span>
+        <span className="opacity-50">|</span>
+        <span className="font-en text-xl tracking-wide">
+          Late Night
+        </span>
+      </Link>
       <nav aria-label="Primary">
         <ul className="flex flex-row gap-4 items-center">
           <li><Link href="/">Home</Link></li>
