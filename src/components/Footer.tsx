bfae14dd91034b9f18d83fd665f3401f0587a973
netakiryosuke <sigoryosuke@gmail.com>
2026-04-18 19:32:34 +0900

    Add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <span>netakiri-bed</span>
+      <span>Written from my bed.</span>
+      <span>© 2026</span>
+    </footer>
+  );
+}
