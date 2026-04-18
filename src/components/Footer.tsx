5b1afefd7e5cdcc51e855adaa3bb81ad10e98a21
netakiryosuke <sigoryosuke@gmail.com>
2026-04-18 19:36:26 +0900

    Update Footer text styles and copyright information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import styles from "./Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <span>netakiri-bed</span>
+      <span className="text-xl">netakiri-bed</span>
       <span>Written from my bed.</span>
-      <span>© 2026 Your Name</span>
+      <span>© 2026 Ryosuke Osawa</span>
     </footer>
   );
 }
