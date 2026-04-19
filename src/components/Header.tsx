d6750e69b7e94b9351ddf21d789aeef9155a2ba6
Ryosuke Osawa <sigoryosuke@gmail.com>
2026-04-20 03:27:24 +0900

    Fix about page rendering (#27)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,13 +11,13 @@ interface Props {
 
 export default function Header({ tags }: Props) {
 
-  const handleHomeClick = () => {
+  const handleClick = () => {
     window.scrollTo(0, 0);
   };
 
   return (
     <header className={styles.header}>
-      <Link href="/" className="flex gap-2" onClick={handleHomeClick}>
+      <Link href="/" className="flex gap-2" onClick={handleClick}>
         <span className="font-jp text-xl">寝たきり</span>
         <span aria-hidden="true" className="opacity-50">|</span>
         <span className="font-en text-xl tracking-wide">
@@ -28,12 +28,12 @@ export default function Header({ tags }: Props) {
       <nav aria-label="Primary" className="hidden md:block">
         <ul className="flex flex-row gap-4 items-center text-xl font-en">
           <li>
-            <Link href="/" onClick={handleHomeClick}>
+            <Link href="/" onClick={handleClick}>
               Home
             </Link>
           </li>
           <li><TagsDropdown tags={tags} /></li>
-          <li><a href="/about">About</a></li>
+          <li><Link href="/about" onClick={handleClick}>About</Link></li>
           <li><a href="https://github.com/netakiryosuke/netakiri-bed" target="_blank" rel="noopener noreferrer">GitHub</a></li>
         </ul>
       </nav>
